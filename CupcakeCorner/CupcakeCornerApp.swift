7cd3eba578e2d8bb5f2246de2ff3d816b4801bf2
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-27 22:41:43 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Mete Alp Kizilcay on 27.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
